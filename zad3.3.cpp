8e4a0326c4e3140fe415d8270ae4e9c5c8efbf12
Patryk-Kosmider <115877789+Patryk-Kosmider@users.noreply.github.com>
2022-11-12 19:28:06 +0100

    Update zad3.3.cpp

--- zad3.3.cpp
+++ zad3.3.cpp
@@ -23,21 +23,46 @@ int main()
 	cin >> c;
 
 
-	cout << "Twoje rownanie kwadratowe to: " << a << "x^2 + " << b << "x + " << c << endl;
+	if (b > 0 && c > 0)
+	{
+		cout << "Twoje rownanie kwadratowe to: " << a << "x^2+" << b << "x+" <<
+			c << endl;
+	}
+	else if (b > 0 && c < 0)
+	{
+		cout << "Twoje rownanie kwadratowe to: " << a << "x^2+" << b << "x" << c << endl;
+	}
+	else if (b < 0 && c > 0)
+	{
+		cout << "Twoje rownanie kwadratowe to: " << a << "x^2" << b << "x+" << c << endl;
+	}
+	else if (b < 0 && c < 0)
+	{
+		cout << "Twoje rownanie kwadratowe to: " << a << "x^2" << b << "x" <<
+			c << endl;
+	}
+	else
+	{
+		cout << "Twoje rownanie kwadratowe to: " << a << "x^2 + " << b << "x + " << c << endl;
+	}
+
+
 
 	float delta;
 
 	delta = (b * b) - (4 * a * c);
 
 	float sqrtdel = sqrt(delta);
-	if(delta < 0)
+	if (delta < 0)
 	{
 		cout << "Pierwiastki nie istnieja, delta jest ujemna" << endl;
-	} else if(delta == 0)
+	}
+	else if (delta == 0)
 	{
 		p0 = (b * -1) / (2 * a);
 		cout << "Delta ma jeden pierwiastek rowny: " << p0 << endl;
-	}else
+	}
+	else
 	{
 		p1 = ((b * -1) - sqrtdel) / (2 * a);
 		p2 = ((b * -1) + sqrtdel) / (2 * a);
